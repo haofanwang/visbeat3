26934aed78cb1d9bcde331fa05d6783eeb7b8a7d
wangqixun <253817124@qq.com>
2022-03-22 18:57:21 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # visbeat3
-visbeat for Python3
+visbeat for Python3.
+
+Code for making anything dance to anything.
+Based on "Visual Rhythm and Beat" SIGGRAPH 2018, Abe Davis and Maneesh Agrawala
+
+Original Project Website: http://abedavis.com/visualbeat/
+
+
+You can install using 
+`pip install visbeat3`
+
+The easiest way to get started is probably to use the docker file. It will install everything and run a jupyter notebook for you in the examples folder. If you have trouble getting things to run on your own machine, the docker file may also provide guidance.
+ 
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="visbeat3",
+  version="0.0.1",
+  author="Haofan Wang",
+  author_email="haofanwang.ai@gmail.com",
+  description="Code for 'Visual Rhythm and Beat' SIGGRAPH 2018",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/haofanwang/visbeat3",
+  packages=setuptools.find_packages(),
+  install_requires=[
+        'numpy',
+        'scipy',
+        'bs4',
+        'librosa',
+        'imageio',
+        'requests',
+        'moviepy',
+        'termcolor',
+        'youtube-dl',
+        'matplotlib',
+    ],
+  scripts=['bin/dancefer'],
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: Apache Software License",
+  "Operating System :: OS Independent",
+  ],
+)
